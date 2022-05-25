ea8b2891e7c336193b349f360b9974ec36bd355d
fengwei2002 <konng0120@gmail.com>
2022-05-25 22:18:23 +0800

    function compoent to ES6 compoent

--- src/index.js
+++ src/index.js
@@ -4,19 +4,34 @@ import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+// function Clock(props) {
+//   return (
+//     <div>
+//       <h1>Hello World</h1>
+//       <h2>It is {props.date.toLocaleTimeString()}</h2>
+//     </div>
+//   )
+// } // function compoent 
 
-function Welcome(props) {
-  return (<h1> hello {props.name} </h1>);
+class Clock extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { date: new Date() }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}</h2>
+      </div>
+    )
+  }
+} // ES6 compoent
+
+
+function tick() {
+  root.render(<Clock />)
 }
 
-function App() {
-  return (
-    <div>
-      <Welcome name="fengwei" />
-      <Welcome name="fengdong" />
-      <Welcome name="yxc" />
-    </div>
-  );
-}
-
-root.render(<App />)
+tick()
